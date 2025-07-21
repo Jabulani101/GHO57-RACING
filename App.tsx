2eb984fc7bab4e22789c427c6ce6821f5163e3f1
Jabulani101 <86256586+Jabulani101@users.noreply.github.com>
2025-07-21 19:05:11 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RacingEngine from './components/RacingEngine';
+
+export default function App() {
+  return <RacingEngine />;
+}
---
+++ RacingEngine.tsx
@@ -0,0 +1,1 @@
+/* Placeholder: Full RacingEngine.tsx from earlier */
