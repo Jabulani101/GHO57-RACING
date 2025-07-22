52ab3fc2a48345869e5671891f1016f2e9d1c55a
Jabulani101 <86256586+Jabulani101@users.noreply.github.com>
2025-07-22 11:34:53 +0200

    Delete index.tsx

--- index.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from 'next/dynamic';
-const RacingEngine = dynamic(() => import('../components/RacingEngine'), { ssr: false });
-export default function Home() {
-  return <RacingEngine />;
-}
